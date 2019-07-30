330578bb99a6e44b51561d21777ebe26973f14b7
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-30 03:11:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.select do |word|
+  array.any? do |word|
     word.length > 4
   end
 end
